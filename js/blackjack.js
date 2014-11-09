601202567bbbad8d01d37132a6b3f167f47e4c78
Halsell <crhalsell@chalsell-mbp.local>
2014-11-09 15:18:54 -0800

    added player chip bet area

--- js/blackjack.js
+++ js/blackjack.js
@@ -6,6 +6,9 @@ GAMES.Blackjack = (function() {
 	var playerBet = 0;
 	var playerBalance = 500; // starting balance
 
+	// cache selectors
+
+
 	var Card = function(position, name, value) {
 		this.position = position;
 		this.name = name;
@@ -13,57 +16,57 @@ GAMES.Blackjack = (function() {
 	}
 
 	var card00 = new Card('card-2h', '2 of Hearts', 2),
-		card01 = new Card('card-2d', '2 of Diamonds', 2),
-		card02 = new Card('card-2c', '2 of Clubs', 2),
-		card03 = new Card('card-2s', '2 of Spades', 2),
-		card04 = new Card('card-3h', '3 of Hearts', 3),
-		card05 = new Card('card-3d', '3 of Diamonds', 3),
-		card06 = new Card('card-3c', '3 of Clubs', 3),
-		card07 = new Card('card-3s', '3 of Spades', 3),
-		card08 = new Card('card-4h', '4 of Hearts', 4),
-		card09 = new Card('card-4d', '4 of Diamonds', 4),
-		card10 = new Card('card-4c', '4 of Clubs', 4),
-		card11 = new Card('card-4s', '4 of Spades', 4),
-		card12 = new Card('card-5h', '5 of Hearts', 5),
-		card13 = new Card('card-5d', '5 of Diamonds', 5),
-		card14 = new Card('card-5c', '5 of Clubs', 5),
-		card15 = new Card('card-5s', '5 of Spades', 5),
-		card16 = new Card('card-6h', '6 of Hearts', 6),
-		card17 = new Card('card-6d', '6 of Diamonds', 6),
-		card18 = new Card('card-6c', '6 of Clubs', 6),
-		card19 = new Card('card-6s', '6 of Spades', 6),
-		card20 = new Card('card-7h', '7 of Hearts', 7),
-		card21 = new Card('card-7d', '7 of Diamonds', 7),
-		card22 = new Card('card-7c', '7 of Clubs', 7),
-		card23 = new Card('card-7s', '7 of Spades', 7),
-		card24 = new Card('card-8h', '8 of Hearts', 8),
-		card25 = new Card('card-8d', '8 of Diamonds', 8),
-		card26 = new Card('card-8c', '8 of Clubs', 8),
-		card27 = new Card('card-8s', '8 of Spades', 8),
-		card28 = new Card('card-9h', '9 of Hearts', 9),
-		card29 = new Card('card-9d', '9 of Diamonds', 9),
-		card30 = new Card('card-9c', '9 of Clubs', 9),
-		card31 = new Card('card-9s', '9 of Spades', 9),
-		card32 = new Card('card-10h', '10 of Hearts', 10),
-		card33 = new Card('card-10d', '10 of Diamonds', 10),
-		card34 = new Card('card-10c', '10 of Clubs', 10),
-		card35 = new Card('card-10s', '10 of Spades', 10),
-		card36 = new Card('card-jh', 'Jack of Hearts', 10),
-		card37 = new Card('card-jd', 'Jack of Diamonds', 10),
-		card38 = new Card('card-jc', 'Jack of Clubs', 10),
-		card39 = new Card('card-js', 'Jack of Spades', 10),
-		card40 = new Card('card-qh', 'Queen of Hearts', 10),
-		card41 = new Card('card-qd', 'Queen of Diamonds', 10),
-		card42 = new Card('card-qc', 'Queen of Clubs', 10),
-		card43 = new Card('card-qs', 'Queen of Spades', 10),
-		card44 = new Card('card-kh', 'King of Hearts', 10),
-		card45 = new Card('card-kd', 'King of Diamonds', 10),
-		card46 = new Card('card-kc', 'King of Clubs', 10),
-		card47 = new Card('card-ks', 'King of Spades', 10),
-		card48 = new Card('card-ah', 'Ace of Hearts', 11),
-		card49 = new Card('card-ad', 'Ace of Diamonds', 11),
-		card50 = new Card('card-ac', 'Ace of Clubs', 11),
-		card51 = new Card('card-as', 'Ace of Spades', 11);
+			card01 = new Card('card-2d', '2 of Diamonds', 2),
+			card02 = new Card('card-2c', '2 of Clubs', 2),
+			card03 = new Card('card-2s', '2 of Spades', 2),
+			card04 = new Card('card-3h', '3 of Hearts', 3),
+			card05 = new Card('card-3d', '3 of Diamonds', 3),
+			card06 = new Card('card-3c', '3 of Clubs', 3),
+			card07 = new Card('card-3s', '3 of Spades', 3),
+			card08 = new Card('card-4h', '4 of Hearts', 4),
+			card09 = new Card('card-4d', '4 of Diamonds', 4),
+			card10 = new Card('card-4c', '4 of Clubs', 4),
+			card11 = new Card('card-4s', '4 of Spades', 4),
+			card12 = new Card('card-5h', '5 of Hearts', 5),
+			card13 = new Card('card-5d', '5 of Diamonds', 5),
+			card14 = new Card('card-5c', '5 of Clubs', 5),
+			card15 = new Card('card-5s', '5 of Spades', 5),
+			card16 = new Card('card-6h', '6 of Hearts', 6),
+			card17 = new Card('card-6d', '6 of Diamonds', 6),
+			card18 = new Card('card-6c', '6 of Clubs', 6),
+			card19 = new Card('card-6s', '6 of Spades', 6),
+			card20 = new Card('card-7h', '7 of Hearts', 7),
+			card21 = new Card('card-7d', '7 of Diamonds', 7),
+			card22 = new Card('card-7c', '7 of Clubs', 7),
+			card23 = new Card('card-7s', '7 of Spades', 7),
+			card24 = new Card('card-8h', '8 of Hearts', 8),
+			card25 = new Card('card-8d', '8 of Diamonds', 8),
+			card26 = new Card('card-8c', '8 of Clubs', 8),
+			card27 = new Card('card-8s', '8 of Spades', 8),
+			card28 = new Card('card-9h', '9 of Hearts', 9),
+			card29 = new Card('card-9d', '9 of Diamonds', 9),
+			card30 = new Card('card-9c', '9 of Clubs', 9),
+			card31 = new Card('card-9s', '9 of Spades', 9),
+			card32 = new Card('card-10h', '10 of Hearts', 10),
+			card33 = new Card('card-10d', '10 of Diamonds', 10),
+			card34 = new Card('card-10c', '10 of Clubs', 10),
+			card35 = new Card('card-10s', '10 of Spades', 10),
+			card36 = new Card('card-jh', 'Jack of Hearts', 10),
+			card37 = new Card('card-jd', 'Jack of Diamonds', 10),
+			card38 = new Card('card-jc', 'Jack of Clubs', 10),
+			card39 = new Card('card-js', 'Jack of Spades', 10),
+			card40 = new Card('card-qh', 'Queen of Hearts', 10),
+			card41 = new Card('card-qd', 'Queen of Diamonds', 10),
+			card42 = new Card('card-qc', 'Queen of Clubs', 10),
+			card43 = new Card('card-qs', 'Queen of Spades', 10),
+			card44 = new Card('card-kh', 'King of Hearts', 10),
+			card45 = new Card('card-kd', 'King of Diamonds', 10),
+			card46 = new Card('card-kc', 'King of Clubs', 10),
+			card47 = new Card('card-ks', 'King of Spades', 10),
+			card48 = new Card('card-ah', 'Ace of Hearts', 11),
+			card49 = new Card('card-ad', 'Ace of Diamonds', 11),
+			card50 = new Card('card-ac', 'Ace of Clubs', 11),
+			card51 = new Card('card-as', 'Ace of Spades', 11);
 
 	deck = [card00, card01, card02, card03, card04, card05, card06, card07, card08, card09,
 			card10, card11, card12, card13, card14, card15, card16, card17, card18, card19,
@@ -124,10 +127,6 @@ GAMES.Blackjack = (function() {
 	var dealerCard = function() {
 		return '<div id="dealerCard' + dealerCounter + '" class="card '+ deck[deckCounter].position + '"></div>'
 	}
-
-
-	//var playerInitCards = '<div id="playerCard1" class="card"></div> <div id="playerCard2" class="card"></div>'
-	//var dealerInitCards = '<div id="dealerCard1" class="card overturn"></div>	<div id="dealerCard2" class="card"></div>'
 
 	// deal the first four cards
 	var deal = function() {
@@ -211,9 +210,7 @@ GAMES.Blackjack = (function() {
 			$('#status').html("You Win!");
 			playerBalance = playerBalance + (playerBet * 2);
 			playerBet = 0;
-			updatePlayerScoreboard();		
-			// $('#btnBet').removeClass('hide');
-			// $('#btnHit, #btnHold').addClass('hide');
+			updatePlayerScoreboard();
 
 		} else if (dealerScore <= 21) {
 			 if (playerScore > dealerScore) {
@@ -222,16 +219,12 @@ GAMES.Blackjack = (function() {
 				playerBalance = playerBalance + (playerBet * 2);
 				playerBet = 0;
 				updatePlayerScoreboard();
-				// $('#btnBet').removeClass('hide');
-				// $('#btnHit, #btnHold').addClass('hide');
 
 			} else if (playerScore < dealerScore) {
 				hideHit();
 				$('#status').html("You lose! Good day, sir!");
 				playerBet = 0;
 				updatePlayerScoreboard();
-				// $('#btnBet').removeClass('hide');
-				// $('#btnHit, #btnHold').addClass('hide');
 
 			} else {
 				hideHit();
@@ -239,8 +232,6 @@ GAMES.Blackjack = (function() {
 				playerBalance = playerBalance + playerBet;
 				playerBet = 0;
 				updatePlayerScoreboard();
-				//$('#btnBet').removeClass('hide');
-				//$('#btnHit, #btnHold').addClass('hide');
 			}
 		}
 
@@ -277,6 +268,7 @@ GAMES.Blackjack = (function() {
 	};
 
 	var chipBet = function (chipValue) {
+		// if (chipValue <= playerBalance && (playerBet + chipValue) <= 500) {
 		if (chipValue <= playerBalance && (playerBet + chipValue) <= 500) {
 			playerBet += chipValue;
 			playerBalance -= chipValue;
